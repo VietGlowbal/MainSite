cba68d6149467f290be332020af6cf6d56994a97
Auerlian <jxl1774@student.bham.ac.uk>
2026-06-19 23:01:31 +0100

    style: remove language switch arrow and clean up collapsed flag layout

--- src/components/nav-reveal.tsx
+++ src/components/nav-reveal.tsx
@@ -239,11 +239,6 @@ function LanguageSwitcher() {
           </>
         )}
       </span>
-      <span className="glowbal-language-arrow">
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <polyline points="6 9 12 15 18 9" />
-        </svg>
-      </span>
     </button>
   );
 }
